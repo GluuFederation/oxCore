31edc6711808389c2acd54279a587d7490925503
yuriyz <yzabrovarniy>
2018-08-09 22:47:14 +0200

    oxcore : added more verbose logging to NativerPersistenceCacheProvider to easier trace problems.
    https://github.com/GluuFederation/oxAuth/issues/654

--- oxService/src/main/java/org/xdi/service/cache/NativePersistenceCacheProvider.java
+++ oxService/src/main/java/org/xdi/service/cache/NativePersistenceCacheProvider.java
@@ -118,7 +118,7 @@ public class NativePersistenceCacheProvider extends AbstractCacheProvider<LdapEn
 
             ldapEntryManager.persist(entity);
         } catch (Exception e) {
-            log.trace("Failed to put entry, key: " + originalKey + ", hashedKey: " + key); // log as trace since it is perfectly valid that entry is removed by timer for example
+            log.trace("Failed to put entry, key: " + originalKey + ", hashedKey: " + key + ", message: " + e.getMessage(), e); // log as trace since it is perfectly valid that entry is removed by timer for example
         }
     }
 
@@ -141,7 +141,7 @@ public class NativePersistenceCacheProvider extends AbstractCacheProvider<LdapEn
             key = hashKey(key);
             ldapEntryManager.removeWithSubtree(createDn(key));
         } catch (Exception e) {
-            log.trace("Failed to remove entry, key: " + originalKey + ", hashedKey: " + key); // log as trace since it is perfectly valid that entry is removed by timer for example
+            log.trace("Failed to remove entry, key: " + originalKey + ", hashedKey: " + key + ", message: " + e.getMessage(), e); // log as trace since it is perfectly valid that entry is removed by timer for example
         }
     }
 

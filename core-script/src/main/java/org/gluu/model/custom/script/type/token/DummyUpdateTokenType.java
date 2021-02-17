ef98d5f0ea68b7038df0f000670e64b880b872b1
yurem <Yuriy.Movchan@gmail.com>
2021-02-17 21:23:17 +0300

    Add license

--- core-script/src/main/java/org/gluu/model/custom/script/type/token/DummyUpdateTokenType.java
+++ core-script/src/main/java/org/gluu/model/custom/script/type/token/DummyUpdateTokenType.java
@@ -1,3 +1,9 @@
+/*
+ * oxCore is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
+ *
+ * Copyright (c) 2020, Gluu
+ */
+
 package org.gluu.model.custom.script.type.token;
 
 import java.util.Map;

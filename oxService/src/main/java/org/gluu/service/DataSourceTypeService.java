46be63736a570c3c09487a46a3421d93b49656e0
yurem <Yuriy.Movchan@gmail.com>
2021-06-01 09:19:05 +0300

    feat: add method to determine if DB is Spanner

--- oxService/src/main/java/org/gluu/service/DataSourceTypeService.java
+++ oxService/src/main/java/org/gluu/service/DataSourceTypeService.java
@@ -1,21 +1,25 @@
 package org.gluu.service;
 
-import org.gluu.persist.PersistenceEntryManager;
-import org.gluu.persist.ldap.impl.LdapEntryManagerFactory;
-
 import java.io.Serializable;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.inject.Named;
+
+import org.gluu.persist.PersistenceEntryManager;
+import org.gluu.persist.cloud.spanner.impl.SpannerEntryManagerFactory;
+import org.gluu.persist.ldap.impl.LdapEntryManagerFactory;
 
 @ApplicationScoped
-@Named
 public class DataSourceTypeService implements Serializable {
 
     private static final long serialVersionUID = -1941135478226842653L;
 
     @Inject
     private PersistenceEntryManager entryManager;
+
+    public boolean isSpanner(String key) {
+        return entryManager.getPersistenceType(key).equals(SpannerEntryManagerFactory.PERSISTENCE_TYPE);
+    }
 
     public boolean isLDAP(String key) {
         return entryManager.getPersistenceType(key).equals(LdapEntryManagerFactory.PERSISTENCE_TYPE);

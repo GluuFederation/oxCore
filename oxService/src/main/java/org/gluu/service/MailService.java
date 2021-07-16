312c7d60a9fd08c5e4fa355be53928ff03f87413
yurem <Yuriy.Movchan@gmail.com>
2021-07-16 14:33:46 +0300

    feat: upgrade libs

--- oxService/src/main/java/org/gluu/service/MailService.java
+++ oxService/src/main/java/org/gluu/service/MailService.java
@@ -2,7 +2,8 @@
  * oxCore is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
  *
  * Copyright (c) 2014, Gluu
- */package org.gluu.service;
+ */
+package org.gluu.service;
 
 import java.util.Date;
 import java.util.Properties;

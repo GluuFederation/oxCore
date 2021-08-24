79d43a503f07e442b47059917844966681bc0aa3
yurem <Yuriy.Movchan@gmail.com>
2021-08-24 13:52:14 +0300

    feat: merge changes from 4.3.0

--- persistence-annotation/src/main/java/org/gluu/persist/annotation/Expiration.java
+++ persistence-annotation/src/main/java/org/gluu/persist/annotation/Expiration.java
@@ -6,19 +6,23 @@
 
 package org.gluu.persist.annotation;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
 /**
- * Persistance Expiration
+ * Persistance Entry Expiration
  *
  * @author Yuriy Movchan Date: 03/26/2020
  */
-@Target({ FIELD })
-@Retention(RUNTIME)
+@Target({ ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Expiration {
+    /**
+     * (Optional) Specify that we ignore this Persistance attribute during update.
+     * Defaults value is false.
+     */
     boolean ignoreDuringUpdate() default false;
+
 }
--- persistence-core/src/main/java/org/gluu/persist/impl/BaseEntryManager.java
+++ persistence-core/src/main/java/org/gluu/persist/impl/BaseEntryManager.java
@@ -93,7 +93,7 @@ public abstract class BaseEntryManager implements PersistenceEntryManager {
 
 		Object dnValue = getDNValue(entry, entryClass);
 
-		Integer expirationValue = getExpirationValue(entry, entryClass);
+		Integer expirationValue = getExpirationValue(entry, entryClass, false);
 
 		List<AttributeData> attributes = getAttributesListForPersist(entry, propertiesAnnotations);
 

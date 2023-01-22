24e3841d5a8b460c880c3c8c1a29f6908b2f27ce
shekhar16 <shekharlaad1609@gmail.com>
2023-01-23 02:03:43 +0530

    feat(oxtrust-server): added audit log location property

--- oxService/src/main/java/org/gluu/model/LocaleSupported.java
+++ oxService/src/main/java/org/gluu/model/LocaleSupported.java
@@ -1,5 +1,7 @@
 package org.gluu.model;
 
+import java.util.Objects;
+
 public class LocaleSupported {	
 	
 	private String locale;
@@ -19,11 +21,34 @@ public class LocaleSupported {
 	public void setLocale(String locale) {
 		this.locale = locale;
 	}
+
 	public String getDisplayName() {
 		return displayName;
 	}
 	public void setDisplayName(String displayName) {
 		this.displayName = displayName;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(displayName, locale);
+	}
+
+	@Override
+	public String toString() {
+		return "LocaleSupported [locale=" + locale + ", displayName=" + displayName + "]";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LocaleSupported other = (LocaleSupported) obj;
+		return Objects.equals(displayName, other.displayName) && Objects.equals(locale, other.locale);
 	}
 
 }
